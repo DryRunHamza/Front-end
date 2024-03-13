27bb68bfe1aa77f54720b4b3902a32e97399a062
Hamza Ahmed <87394797+DryRunHamza@users.noreply.github.com>
2024-03-13 13:22:51 +0500

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+// document.getElementById("demo").innerHTML = "Hello World!";
+
+// document.getElementById("demo").innerHTML = "first text!"
+
+// document.getElementById("demo").innerHTML = "second!"
